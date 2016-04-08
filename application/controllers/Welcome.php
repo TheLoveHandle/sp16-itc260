676009e50cdeb37bb427dda34bf62d75925e5938
TheLoveHandle <adanie04@seattlecentral.edu>
2016-04-07 17:41:12 -0700

    different way of doing includes

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,7 +7,9 @@ class Welcome extends CI_Controller {
 	{
         $data['title'] = "My unique title tag!";
         $data['page_id'] = "My Clever Page id";
-        $data['guts'] = '<p>I am the creamy filling inside!</p>';
-		$this->load->view('welcome_message', $data);
-	}
+        $data['guts'] = '<p>I am still the creamy filling inside!</p>';
+		$this->load->view('templates/header', $data);
+        $this->load->view('welcome_message', $data);
+        $this->load->view('templates/footer', $data);
+    }
 }
