1b8a0f71d43074aa9efa9f2b8830995f3a14f847
TheLoveHandle <adanie04@seattlecentral.edu>
2016-04-19 17:48:01 -0700

    commit

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -1,15 +1,13 @@
 <?php
 class News extends CI_Controller {
 
-        public function __construct()
-        {
+        public function __construct(){
                 parent::__construct();
                 $this->load->model('news_model');
                 $this->load->helper('url_helper');
         }
 
-        public function index()
-        {
+        public function index(){
                 $data['news'] = $this->news_model->get_news();
                 $data['page_id'] = 'News';
                 $data['title'] = 'News Archive';
@@ -33,4 +31,52 @@ class News extends CI_Controller {
             $this->load->view('news/view', $data);
             $this->load->view('templates/footer');
         }
-}
+    
+        public function create(){
+            $this->load->helper('form');
+            $this->load->library('form_validation');
+
+            $data['title'] = 'Create a news item';
+            $data['page_id'] = 'News Create';
+
+            $this->form_validation->set_rules('title', 'Title', 'required');
+            $this->form_validation->set_rules('text', 'Text', 'required');
+
+            if ($this->form_validation->run() === FALSE){
+                $this->load->view('templates/header', $data);
+                $this->load->view('news/create', $data);
+                $this->load->view('templates/footer', $data);
+            }
+            else{
+                $slug = $this->news_model->set_news();
+                
+                
+                
+                
+                
+                $data['news_item'] = $this->news_model->get_news($slug);
+                $data['page_id'] = "News";
+
+                if (empty($data['news_item'])){
+                show_404();
+                }
+
+                $data['title'] = $data['news_item']['title'];
+
+                $this->load->view('templates/header', $data);
+                $this->load->view('news/view', $data);
+                $this->load->view('templates/footer');
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        }//end else
+    }//end create function    
+}//end class
--- application/models/News_model.php
+++ application/models/News_model.php
@@ -15,4 +15,32 @@ class News_model extends CI_Model {
     $query = $this->db->get_where('sp16_news', array('slug' => $slug));
     return $query->row_array();
     }
+    
+    public function set_news(){
+        $this->load->helper('url');
+
+        $slug = url_title($this->input->post('title'), 'dash', TRUE);
+echo 'inside news model';
+        die;
+        $data = array(
+            'title' => $this->input->post('title'),
+            'slug' => $slug,
+            'text' => $this->input->post('text')
+        );
+        
+        if($this->db->insert('sp16_news', $data)){
+            //echo $this->db->insert_id();
+            $id = $this->db->insert_id();
+            die;
+            
+            $query = $this->db->get('sp16_news', array('id' => $id));
+            return $query->result_array();
+        }
+        else{
+            echo 'uh oh';
+            die;
+        }
+
+        //return $this->db->insert('sp16_news', $data);
+    }//end function
 }
